5bf7431074b1ab1883adb4a7cbae1cfe22f08714
Dhruvin Pambhar <dhruvinpambhar0609@gmail.com>
2023-03-30 19:50:30 +0530

    Initial commit

--- cricket/urls.py
+++ cricket/urls.py
@@ -7,9 +7,11 @@ urlpatterns = [
     path("", views.home, name="home"),
     path("logout", views.logout_request, name="logout"),
     path('list', views.list_view, name='list_teams'),
-    path('player_name', views.addplayer_name, name='player_name'),
+    # path('player_name', views.addplayer_name, name='player_name'),
     path('team_name', views.team_name, name='team_name'),
     path("newmatch", views.addnewmatch, name="newmatch"),
     path("update_score", views.update_score, name="update_score"),
-    path("extrarun", views.extrarun, name="extrarun")
+    path("extrarun", views.extrarun, name="extrarun"),
+    path("matchdetail", views.matchdetail, name="matchdetail"),
+    path("plyerdetails", views.plyerdetails, name="plyerdetails"),
 ]
--- cricketwallh/urls.py
+++ cricketwallh/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('cricket/', include('cricket.urls')),
+    # path('admin/', admin.site.urls),
+    path('', include('cricket.urls')),
 ]
